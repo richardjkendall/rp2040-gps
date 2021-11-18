b5fe52fae78bf08c8a5e7c9746c742ebf4610b13
Richard Kendall <richard.kendall@gmail.com>
2021-11-18 19:48:32 +1100

    basic support for patterns now added

--- core1.c
+++ core1.c
@@ -14,21 +14,29 @@
 const struct Led led1 = { .redpin = LED1_RED, .greenpin = LED1_GRN, .bluepin = LED1_BLU };
 const struct Led led2 = { .redpin = LED2_RED, .greenpin = LED2_GRN, .bluepin = LED2_BLU };
 
-const struct Colour colours[] = {
-  { .red = 1024, .green = 0, .blue = 0 },     // red
-  { .red = 1024, .green = 1024, .blue = 0 },     // yellow
-  { .red = 0, .green = 1024, .blue = 0 },     // green
-  { .red = 0, .green = 1024, .blue = 1024 },  // turq
-  { .red = 1024, .green = 1024, .blue = 1024 },     // white
-  { .red = 0, .green = 0, .blue = 1024 },     // blue
-  { .red = 1024, .green = 0, .blue = 1024 }  // purple
-}; 
+const struct Colour red = { .red = 1024, .green = 0, .blue = 0 };
+const struct Colour yellow = { .red = 1024, .green = 1024, .blue = 0 };
+const struct Colour green = { .red = 0, .green = 1024, .blue = 0 };
+const struct Colour turq = { .red = 0, .green = 1024, .blue = 1024 };
+const struct Colour white = { .red = 1024, .green = 1024, .blue = 1024 };
+const struct Colour blue = { .red = 0, .green = 0, .blue = 1024 };
+const struct Colour purple = { .red = 1024, .green = 0, .blue = 1024 };
+const struct Colour orange = { .red = 1024, .green = 512, .blue = 0 };
+const struct Colour black = { .red = 0, .green = 0, .blue = 0 };
 
-struct Colour from;
-struct Colour to;
+const struct Pattern patterns[] = {
+  { .steps = 2, .stepdelay = 2, .led1colours = { red, green }, .led2colours = { green, red } }
+};
+
+struct Colour from_led1;
+struct Colour to_led1;
+struct Colour from_led2;
+struct Colour to_led2;
 
 int step = 0;
 int col = 0;
+int pattern = 0;
+int pattern_step = 0;
 
 // Core 1 interrupt Handler
 void core1_interrupt_handler() {
@@ -38,20 +46,31 @@ void core1_interrupt_handler() {
     uint32_t sec = raw & 63;
     uint32_t min = (raw >> 6) & 63;
     uint32_t hr = (raw >> 12) & 31;
-    //printf("Recovered time %d:%d:%d\n", hr, min, sec);  
-    if(sec % 2 == 0) {
-      if(col == 6) {
-        col = 0;
-        from = colours[6];
-        to = colours[0];
+    printf("Recovered time %d:%d:%d\n", hr, min, sec);  
+
+    if (sec % patterns[pattern].stepdelay == 0) {
+      if(pattern_step == patterns[pattern].steps) {
+        // need to reset the step count to 0
+        to_led1 = patterns[pattern].led1colours[0];
+        from_led1 = patterns[pattern].led1colours[patterns[pattern].steps - 1];
+
+        to_led2 = patterns[pattern].led2colours[0];
+        from_led2 = patterns[pattern].led2colours[patterns[pattern].steps - 1];
+
+        pattern_step = 1;
         step = 0;
       } else {
-        col++;
-        from = colours[col-1];
-        to = colours[col];
+        to_led1 = patterns[pattern].led1colours[pattern_step];
+        from_led1 = patterns[pattern].led1colours[pattern_step - 1];
+
+        to_led2 = patterns[pattern].led2colours[pattern_step];
+        from_led2 = patterns[pattern].led2colours[pattern_step - 1];
+
+        pattern_step++;
         step = 0;
       }
     }
+    
   }
   multicore_fifo_clear_irq(); // Clear interrupt
 }
@@ -69,10 +88,15 @@ void core1_entry() {
   setup_pwm(led2, MAX_PWM);
 
   while(true) {
+    // get max step count
+    int maxstep = patterns[pattern].stepdelay * 50;
+
     // handle led1
-    fade_between(led1, from, to, 50, step, MAX_PWM);
+    fade_between(led1, from_led1, to_led1, maxstep, step, MAX_PWM);
+
     // handle led2
-    fade_between(led2, to, from, 50, step, MAX_PWM);
+    fade_between(led2, from_led2, to_led2, maxstep, step, MAX_PWM);
+
     step++;
     sleep_ms(20);
     //tight_loop_contents();
--- pwm.h
+++ pwm.h
@@ -10,6 +10,13 @@ struct Colour {
   int blue;
 };
 
+struct Pattern {
+  int steps;
+  int stepdelay;
+  struct Colour led1colours[10];
+  struct Colour led2colours[10];
+};
+
 void setup_pwm(struct Led led, int limit);
 void set_led(struct Colour colour, struct Led led, int limit);
 void fade_between(struct Led led, struct Colour from, struct Colour to, int steps, int step, int limit);
